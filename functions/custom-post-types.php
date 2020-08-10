f678f30744cbaac6df5a2502bdec3aa968452bdc
Jenn Schlick <jschlick@mit.edu>
2020-08-10 11:24:07 -0400

    Update custom post types function.

--- functions/custom-post-types.php
+++ functions/custom-post-types.php
@@ -4,20 +4,34 @@
 /*-----------------------------------------------------------------------------------*/
 
 // function basetheme_custom_post_types() {
+// 	$labels = array(
+// 		'name' => __('Lorem Ipsums'),
+// 		'singular_name' => __('Lorem Ipsum'),
+// 		'add_new' => __('Add New'),
+// 		'add_new_item' => __('Add Lorem Ipsum'),
+// 		'edit_item' => __('Edit Lorem Ipsum'),
+// 		'new_item' => __('Add Lorem Ipsum'),
+// 		'view_item' => __('View Lorem Ipsum'),
+// 		'search_items' => __('Search Lorem Ipsum'),
+// 		'not_found' =>  __('Nothing found'),
+// 		'not_found_in_trash' => __('Nothing found'),
+// 		'parent_item_colon' => ''
+// 	);
+//
 // 	$args = array(
 // 		'labels' => $labels,
-// 		'public' => true,
+// 		'public' => false,
 // 		'publicly_queryable' => true,
 // 		'menu_icon' => 'dashicons-admin-page',
 // 		'show_ui' => true,
-// 		'query_var' => true,
-// 		'rewrite' => true,
+// 		'query_var' => false,
+// 		'rewrite' => array('slug' => 'loremipsum'),
 // 		'capability_type' => 'post',
 // 		'hierarchical' => false,
 // 		'supports' => array('title','editor','thumbnail','revisions'),
-// 		'has_archive' => true,
-// 		'rewrite' => array('slug' => 'example'),
+// 		'has_archive' => false
 // 	);
-// 	register_post_type('example', $args);
+//
+// 	register_post_type('loremipsum', $args);
 // }
 // add_action('init', 'basetheme_custom_post_types');
